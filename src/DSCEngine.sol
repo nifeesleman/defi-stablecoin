595db664356c3f1c6db126f39c4575a9b5c8e7a0
nifeesleman <nsr-1868-14@amu.edu.et>
2025-04-15 08:37:35 +0300

    feat: add error DSCEngine__TokenNotAllowed

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -54,27 +54,28 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.
 mapping(address token => address priceFeed) private s_priceFeeds;
 DecentralizedStableCoin private immutable i_dsc;
 mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+
 contract DSCEngine {
-     ///////////////////
+    ///////////////////
     //     Errors    //
     ///////////////////
 
     error DSCEngine__NeedsMoreThanZero();
     error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
-
+    error DSCEngine__TokenNotAllowed();
 
     ///////////////////
     //   Modifiers   //
     ///////////////////
 
-    modifier moreThanZero(uint256 amount){
-        if(amount <=0){
+    modifier moreThanZero(uint256 amount) {
+        if (amount <= 0) {
             revert DSCEngine__NeedsMoreThanZero();
         }
         _;
     }
 
-      modifier isAllowedToken(address token) {
+    modifier isAllowedToken(address token) {
         if (s_priceFeeds[token] == address(0)) {
             revert DSCEngine__TokenNotAllowed(token);
         }
@@ -87,25 +88,30 @@ contract DSCEngine {
     ///////////////////////////
     //   External Functions  //
     ///////////////////////////
-    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress){
-    if(tokenAddresses.length != priceFeedAddresses.length){
-        revert DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+        }
+
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
     }
 
-    for(uint256 i=0; i < tokenAddresses.length; i++){
-        s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
-    }
-    
-
-}
     function depositCollateralAndMintDsc() external {}
-/*
- * @param tokenCollateralAddress: The ERC20 token address of the collateral you're depositing
- * @param amountCollateral: The amount of collateral you're depositing
- */
-    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral) external moreThanZero(amountCollateral) isAllowedToken(tokenCollateralAddress) nonReentrant{
-    s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
-}
+    /*
+    * @param tokenCollateralAddress: The ERC20 token address of the collateral you're depositing
+    * @param amountCollateral: The amount of collateral you're depositing
+    */
+
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+    }
 
     function redeemCollateralForDsc() external {}
 
