57c2d468c426a2ceae16fe466ceb14f56188c2fc
nifeesleman <nsr-1868-14@amu.edu.et>
2025-04-22 11:24:03 +0300

    feat: add DeployDSC script for deploying DecentralizedStableCoin and DSCEngine

---
+++ script/DeployDSC.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract DeployDSC is Script {
+    function run() external returns (DecentralizedStableCoin, DSCEngine) {
+        vm.startBroadcast();
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin(msg.sender);
+        // DSCEngine dscEngine = new DSCEngine(dsc);
+        vm.stopBroadcast();
+    }
+}
