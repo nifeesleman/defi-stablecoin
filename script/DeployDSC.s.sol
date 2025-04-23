72c884b92b43eb12bd4c89fbc6d01e3d1ad1576d
nifeesleman <nsr-1868-14@amu.edu.et>
2025-04-23 12:23:14 +0300

    feat: update DeployDSC script to initialize token and price feed addresses from HelperConfig

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -8,10 +8,22 @@ import {Script} from "forge-std/Script.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract DeployDSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function run() external returns (DecentralizedStableCoin, DSCEngine) {
+        HelperConfig helperConfig = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address wbtc, address weth, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
         vm.startBroadcast();
         DecentralizedStableCoin dsc = new DecentralizedStableCoin(msg.sender);
-        // DSCEngine dscEngine = new DSCEngine(dsc);
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses);
+        dsc.transferOwnership(address(engine));
+
         vm.stopBroadcast();
+        return (dsc, engine);
     }
 }
