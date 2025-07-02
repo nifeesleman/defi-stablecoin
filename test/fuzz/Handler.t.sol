a44ed40987526b3618cf4075edb57cd01415d008
nifeesleman <nsr-1868-14@amu.edu.et>
2025-07-02 19:08:02 +0300

    fix: Update ERC20Mock import path to use local mocks in HelperConfig and Handler contracts

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.18;
 import {Test} from "forge-std/Test.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
-import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {ERC20Mock} from "test/mocks/ERC20Mock.sol";
 import {console} from "forge-std/console.sol";
 import {MockV3Aggregator} from "../mocks/MockV3Aggregator.sol";
 
